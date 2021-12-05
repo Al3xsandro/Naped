9082edaaa234c3f1e49ed14ba8db92d625b30b76
Al3xsandro <alexs123655@gmail.com>
2021-12-05 14:34:42 -0300

    fix: add return null to nprogress

--- src/utils/useNprogress.tsx
+++ src/utils/useNprogress.tsx
@@ -15,18 +15,16 @@ export function Nprogress() {
             NProgress.done()
         }
 
-        router.events.on('routeChangeStart', handleStart)
-        router.events.on('routeChangeComplete', handleStop)
-        router.events.on('routeChangeError', handleStop)
+        router.events.on('routeChangeStart', handleStart);
+        router.events.on('routeChangeComplete', handleStop);
+        router.events.on('routeChangeError', handleStop);
 
         return () => {
-        router.events.off('routeChangeStart', handleStart)
-        router.events.off('routeChangeComplete', handleStop)
-        router.events.off('routeChangeError', handleStop)
+            router.events.off('routeChangeStart', handleStart);
+            router.events.off('routeChangeComplete', handleStop);
+            router.events.off('routeChangeError', handleStop);
         }
     }, [router]);
 
-    return (
-        <></>
-    )
+    return null;
 }
